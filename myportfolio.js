64447349848e852e8bcbbc2e4a1e53a3203494b8
Myrh9n <abubakarigariba9@gmail.com>
2024-08-30 13:36:30 +0000

    changes

---
+++ myportfolio.js
@@ -0,0 +1,38 @@
+//Get the button
+let mybutton = document.getElementById("myBtn");
+
+//When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 120);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toogle('bx-x');
+    navlist.classList.toogle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
